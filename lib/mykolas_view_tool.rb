3b8bac2584a1722bf2a1c1660079be980f3bb896
mykola_levchuk <mlevc@softserve.inc>
2022-06-15 00:28:46 -0400

    implemented custom gem

---
+++ lib/mykolas_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "mykolas_view_tool/version"
+require_relative "mykolas_view_tool/renderer"
+
+module MykolasViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
